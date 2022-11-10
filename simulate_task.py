c2dc2b97d6297c3929d62c8dc6c486d5072b5654
Daniel Kato <daniel.d.kato@gmail.com>
2022-11-10 15:00:48 -0500

    Add plot_model_performances() function

--- simulate_task.py
+++ simulate_task.py
@@ -728,6 +728,28 @@ def plot_perf_v_curv(perf_m, perf_sem, rad_vec, lab_vec=None):
 
 
 
+def plot_model_performances(perf_m, perf_sem, lr_m, lr_sem):
+
+    num_models=perf_m.shape[1]    
+    model_labels=['Linear','NonLin-1','NonLin-2','NonLin-3']
+    alpha_vec=[0.4,0.6,0.8,1.0]
+    width=0.15
+    
+    fig=plt.figure(figsize=(2,2))
+    ax=fig.add_subplot(111)
+    #functions_miscellaneous.adjust_spines(ax,['left','bottom'])
+    ax.plot([-3.5*width,3.5*width],0.5*np.ones(2),color='black',linestyle='--')
+    #plt.xticks(width*np.arange(len(models_vec))-1.5*width,model_labels,rotation='vertical')
+    for j in range(num_models):
+        ax.bar(j*width-1.5*width,perf_m[0,j,1],yerr=perf_sem[0,j,1],color='green',width=width,alpha=alpha_vec[j])
+        #ax.scatter(j*width-1.5*width+p+np.random.normal(0,std_n,3),perf[:,p,j,1],color='black',alpha=alpha_vec[j],s=4)
+    ax.bar(-1.5*width,lr_m[0,1],yerr=lr_sem[0,1],color='green',width=width,alpha=alpha_vec[0])
+    ax.set_ylim([0.4,1.0])
+    #ax.set_xlim([-3.5*width,3.5*width])
+    ax.set_ylabel('Decoding Performance')
+    
+    
+
 def load_hyperparams(hparams):
     """
     Load simulated whisker task parameters and decoder hyperparameters.    
