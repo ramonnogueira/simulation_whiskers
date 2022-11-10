e31fa3cfd52142518afbc2e1604cd24ebc47d807
Daniel Kato <daniel.d.kato@gmail.com>
2022-11-10 14:09:00 -0500

    Change output_directory in example_script.py

--- example_script.py
+++ example_script.py
@@ -9,6 +9,6 @@ from simulation_whiskers.simulate_task import compare_stim_decoders
 
 hparams_path='C:\\Users\\danie\\Documents\\simulation_whiskers\\simulate_task_hparams.json'
 save_figs=True
-output_directory='C:\\Users\\danie\\Documents\\simulation_whiskers\\results'
+output_directory='C:\\Users\\danie\\Documents\\simulation_whiskers\\results\\'
 verbose=True
 compare_stim_decoders(hparams=hparams_path, save_figs=save_figs, output_directory=output_directory, verbose=verbose)
