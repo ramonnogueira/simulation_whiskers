7b894c2c287701a4c6a2d4785f83a360b5fcea4e
Daniel Kato <daniel.d.kato@gmail.com>
2022-12-15 15:17:59 -0500

    stop passing corr to illustrate_stimulus()
    No longer needed

--- simulate_task.py
+++ simulate_task.py
@@ -348,7 +348,7 @@ def illustrate_stimuli(hparams=None, stim=None, n_stim=15, save_figs=False, outp
         curv=np.random.choice(rad_vec,replace=False)
         timem=np.random.choice(steps_mov,replace=False)
 
-        illustrate_stimulus(ax, ind_stim, curv, corr, z1, timem, speed, dt, theta, disp, amp, freq_sh)
+        illustrate_stimulus(ax, ind_stim, curv, z1, timem, speed, dt, theta, disp, amp, freq_sh)
 
         """
         center0=center0_func(curv,z1)[ind_stim] # Center 0
