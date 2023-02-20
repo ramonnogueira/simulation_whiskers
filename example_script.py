4395ac49bd2616604e83cc2e9051b748384ab3ab
Daniel Kato <daniel.d.kato@gmail.com>
2023-02-20 15:12:47 -0500

    Change hparams file

--- example_script.py
+++ example_script.py
@@ -7,7 +7,7 @@ Created on Thu Nov  3 13:16:32 2022
 
 from simulation_whiskers.simulate_task import compare_stim_decoders
 
-hparams_path='C:\\Users\\danie\\Documents\\simulation_whiskers\\example_hparams.json'
+hparams_path='C:\\Users\\danie\\Documents\\simulation_whiskers\\simulate_task_hparams.json'
 save_figs=True
 output_directory='C:\\Users\\danie\\Documents\\simulation_whiskers\\results\\'
 verbose=True
