3b0c4b5167f8739154606569b0693e1f7983995d
Daniel Kato <daniel.d.kato@gmail.com>
2023-03-27 19:30:48 -0400

    Pass task_def to compare_stim_decoders()

--- scripts/example_script.py
+++ scripts/example_script.py
@@ -11,12 +11,13 @@ from simulation_whiskers.simulate_task import compare_stim_decoders
 
 sim_params_path='C:\\Users\\danie\\Documents\\simulation_whiskers\\hyperparams\\example_sim_params.json'
 mlp_hparams_path='C:\\Users\\danie\\Documents\\simulation_whiskers\\hyperparams\\example_mlp_hparams.json'
+task_def_path='C:\\Users\\danie\\Documents\\simulation_whiskers\\hyperparams\\task_defs\\convex_concave.json'
 save_figs=False
 output_directory='C:\\Users\\danie\\Documents\\simulation_whiskers\\results\\'
 verbose=True
 
 start = datetime.now()
-compare_stim_decoders(sim_params=sim_params_path, mlp_hparams=mlp_hparams_path, save_figs=save_figs, output_directory=output_directory, verbose=verbose)
+compare_stim_decoders(sim_params=sim_params_path, mlp_hparams=mlp_hparams_path, task=task_def_path, save_figs=save_figs, output_directory=output_directory, verbose=verbose)
 stop = datetime.now()
 duration = stop - start
 print('Duration = {} seconds'.format(duration.seconds))
