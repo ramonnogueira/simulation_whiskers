a26f8dfc3f0ca7831e06a9788fc40adb5b7aea5e
Daniel Kato <daniel.d.kato@gmail.com>
2023-04-27 13:01:03 -0400

    Add missing if

--- miscellaneous_sparseauto.py
+++ miscellaneous_sparseauto.py
@@ -258,7 +258,7 @@ def iterate_fit_autoencoder(sim_params, autoencoder_params, tasks, n_files, base
     start_time=datetime.now()
     
     n_tasks=len(tasks)
-    n_tasks!=len(sim_params):
+    if n_tasks!=len(sim_params):
         raise AssertionError('len(tasks) must equal len(sim_params).')
     
     # Unpack some autoencoder parameters:
