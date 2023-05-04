527b7017c12f327eb79254fa67a9a4c62e384b5b
Daniel Kato <daniel.d.kato@gmail.com>
2023-05-04 13:28:13 -0400

    Pass actual task labels to test_autoencoder_geometry()

--- miscellaneous_sparseauto.py
+++ miscellaneous_sparseauto.py
@@ -420,10 +420,10 @@ def iterate_fit_autoencoder(sim_params, autoencoder_params, tasks, n_files, mlp_
                 inpt_geo_feat=F
             
             # Test geometry iterating over subsamples to deal with any imbalances in trials per condition:
-            task_inpt_m, ccgp_inpt_m = test_autoencoder_geometry(inpt_geo_feat, Fb, n_geo_subsamples, geo_reg)
-            task_hidden_pre_m, ccgp_hidden_pre_m = test_autoencoder_geometry(hidden_init, Fb, n_geo_subsamples, geo_reg)
-            task_hidden_m, ccgp_hidden_m = test_autoencoder_geometry(hidden_rep, Fb, n_geo_subsamples, geo_reg)
-            task_rec_m, ccgp_rec_m = test_autoencoder_geometry(rec_rep, Fb, n_geo_subsamples, geo_reg)
+            task_inpt_m, ccgp_inpt_m = test_autoencoder_geometry(inpt_geo_feat, test_labels, n_geo_subsamples, geo_reg)
+            task_hidden_pre_m, ccgp_hidden_pre_m = test_autoencoder_geometry(hidden_init, test_labels, n_geo_subsamples, geo_reg)
+            task_hidden_m, ccgp_hidden_m = test_autoencoder_geometry(hidden_rep, test_labels, n_geo_subsamples, geo_reg)
+            task_rec_m, ccgp_rec_m = test_autoencoder_geometry(rec_rep, test_labels, n_geo_subsamples, geo_reg)
             
             """
             # Plot mean data by XOR condition:
