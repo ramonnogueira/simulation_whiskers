76ab803988c3c074e4e52187a4eb351966e6bf55
Daniel Kato <daniel.d.kato@gmail.com>
2023-05-30 15:21:49 -0400

    Fix bugs in forward() method of >1 hidden layer auteoncoders

--- miscellaneous_sparseauto.py
+++ miscellaneous_sparseauto.py
@@ -768,8 +768,8 @@ class sparse_autoencoder_2(sparse_autoencoder):
         self.dec2=torch.nn.Linear(n_hidden,self.k)
         
     def forward(self,x,sigma_noise):
-        x_hidden0 = F.relu(self.h0(x))+sigma_noise*torch.randn(x.size(0),self.n_hidden[0])
-        x_hidden1 = F.relu(self.enc(x_hidden0))+sigma_noise*torch.randn(x_hidden0.size(0),self.n_hidden[1])
+        x_hidden0 = F.relu(self.enc(x))+sigma_noise*torch.randn(x.size(0),self.n_hidden[0])
+        x_hidden1 = F.relu(self.h0(x_hidden0))+sigma_noise*torch.randn(x_hidden0.size(0),self.n_hidden[1])
         x = self.dec(x_hidden1)
         x2 = self.dec2(x_hidden1)
         return x,x_hidden1,x2
@@ -789,9 +789,9 @@ class sparse_autoencoder_3(sparse_autoencoder):
         self.dec2=torch.nn.Linear(n_hidden,self.k)
         
     def forward(self,x,sigma_noise):
-        x_hidden0 = F.relu(self.h0(x))+sigma_noise*torch.randn(x.size(0),self.n_hidden[0])
-        x_hidden1 = F.relu(self.enc(x_hidden0))+sigma_noise*torch.randn(x_hidden0.size(0),self.n_hidden[1])
-        x_hidden2 = F.relu(self.enc(x_hidden1))+sigma_noise*torch.randn(x_hidden1.size(0),self.n_hidden[2])
+        x_hidden0 = F.relu(self.enc(x))+sigma_noise*torch.randn(x.size(0),self.n_hidden[0])
+        x_hidden1 = F.relu(self.h0(x_hidden0))+sigma_noise*torch.randn(x_hidden0.size(0),self.n_hidden[1])
+        x_hidden2 = F.relu(self.h1(x_hidden1))+sigma_noise*torch.randn(x_hidden1.size(0),self.n_hidden[2])
         x = self.dec(x_hidden2)
         x2 = self.dec2(x_hidden2)
         return x,x_hidden2,x2
