8123040a16e426debb8264889a51e22f0aa7c772
Daniel Kato <daniel.d.kato@gmail.com>
2023-05-31 12:40:20 -0400

    Pass xor param to sparse_autoencoder_3()

--- miscellaneous_sparseauto.py
+++ miscellaneous_sparseauto.py
@@ -779,7 +779,7 @@ def ae_dispatch(n_inp,n_hidden,sigma_init,k=[2,2],xor=False):
     elif len(n_hidden)==2:
         ae=sparse_autoencoder_2(n_inp,n_hidden,sigma_init,k=k)
     elif len(n_hidden)==3:
-        ae=sparse_autoencoder_3(n_inp,n_hidden,sigma_init,k=k)
+        ae=sparse_autoencoder_3(n_inp,n_hidden,sigma_init,k=k,xor=xor)
     else:
         raise AssertionError('Invalid number of hidden layers; please select number of hidden layers from 1-3.') 
     return ae
