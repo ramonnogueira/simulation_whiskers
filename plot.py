5acabb26f8baf6e7ad0646087124be91485a2229
Daniel Kato <daniel.d.kato@gmail.com>
2023-09-01 13:19:25 -0400

    Add plot_pars_by_layer function

--- plot.py
+++ plot.py
@@ -378,6 +378,56 @@ def plot_ccgp(task_in, ccgp_in, parallelism_in, plot_train=False, color='blue', 
         ax.scatter(2*width-1.5*width+h_offset,ccgp_m[0],color=color,alpha=alpha_vec[2])
     
     
+
+def plot_pars_by_layer(inpt_par, pre_par, hidden_par, rec_par, save_output=False, output_directory=None):
+
+    # Init:
+    fig=plt.figure(figsize=(4,4))
+    ax=fig.add_subplot(111)
+    offset=0
+    
+    # Plot parallelism score in input:
+    plot_parallelism(inpt_par, color='green', h_offset=offset, ax=ax)
+    offset+=2
+    
+    # Plot parallelism score in hidden layer before training:
+    plot_parallelism(pre_par, color='orange', h_offset=offset, ax=ax)
+    offset+=2
+    
+    # Plot parallelism score in hidden layer:
+    plot_parallelism(hidden_par, color='red', h_offset=offset, ax=ax)
+    offset+=2    
+
+    # Plot geometry of reconstructed output if requested:
+    plot_parallelism(rec_par, color='blue', h_offset=offset, ax=ax)
+    
+    xl=ax.get_xlim()
+    ax.plot([xl[0],xl[1]],0.5*np.ones(2),color='black',linestyle='--')
+    ax.set_ylim([0.4,1.0])
+    ax.set_ylabel('Decoding Performance')
+    
+    # Save figure if requested:
+    if save_output:
+        
+        if output_directory==None:
+            output_directory=os.getcwd()
+        
+        # Create output directory if necessary:
+        if not os.path.exists(output_directory):
+            pathlib.Path(output_directory).mkdir(parents=True, exist_ok=True)
+    
+        output_path=os.path.join(output_directory, 'autoencoeder_geometry.png')
+        fig.savefig(output_path,dpi=500)
+        
+        if 'analysis_metadata' in sys.modules:
+            M=Metadata()         
+            M.add_output(output_path)
+            metadata_path=os.path.join(output_directory, 'plot_autoencoder_geometry_metadata.json')
+            write_metadata(M,metadata_path)
+    
+    return fig    
+    
+
     
 def plot_parallelism(parallelism_in, color='blue', h_offset=0, ax=None):
     
