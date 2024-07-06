0fe485bbf13f871f36be09470335fc40505c9167
Daniel Kato <daniel.d.kato@gmail.com>
2024-07-06 16:24:48 -0400

    Capture dataframes of classifier performance from test_autoencoder_geometry() in iterate_fit_autoencoder()

--- miscellaneous_sparseauto.py
+++ miscellaneous_sparseauto.py
@@ -538,14 +538,14 @@ def iterate_fit_autoencoder(sim_params, tasks, n_files, autoencoder_params=None,
                 inpt_geo_feat=F
             
             # Test geometry iterating over subsamples to deal with any imbalances in trials per condition:
-            task_inpt_m, ccgp_inpt_m, parallel_inpt_m, curr_geo_inpt = test_autoencoder_geometry(inpt_geo_feat, test_labels, n_geo_subsamples, geo_reg)
+            task_inpt_m, ccgp_inpt_m, parallel_inpt_m, curr_perf_inpt, curr_geo_inpt = test_autoencoder_geometry(inpt_geo_feat, test_labels, n_geo_subsamples, geo_reg)
             curr_geo_inpt['layer'] = ['input']*curr_geo_inpt.shape[0]
             curr_geo_df = pd.concat([curr_geo_df, curr_geo_inpt], axis=0)
             
             if autoencoder_params is not None:
-                task_hidden_pre_m, ccgp_hidden_pre_m, parallel_hidden_pre_m, curr_geo_hidden_pre = test_autoencoder_geometry(hidden_init, test_labels, n_geo_subsamples, geo_reg)
-                task_hidden_m, ccgp_hidden_m, parallel_hidden_m, curr_geo_hidden = test_autoencoder_geometry(hidden_rep, test_labels, n_geo_subsamples, geo_reg)
-                task_rec_m, ccgp_rec_m, parallel_rec_m, curr_geo_rec = test_autoencoder_geometry(rec_rep, test_labels, n_geo_subsamples, geo_reg)
+                task_hidden_pre_m, ccgp_hidden_pre_m, parallel_hidden_pre_m, curr_perf_hidden_pre, curr_geo_hidden_pre = test_autoencoder_geometry(hidden_init, test_labels, n_geo_subsamples, geo_reg)
+                task_hidden_m, ccgp_hidden_m, parallel_hidden_m, curr_perf_hidden, curr_geo_hidden = test_autoencoder_geometry(hidden_rep, test_labels, n_geo_subsamples, geo_reg)
+                task_rec_m, ccgp_rec_m, parallel_rec_m, curr_perf_rec, curr_geo_rec = test_autoencoder_geometry(rec_rep, test_labels, n_geo_subsamples, geo_reg)
             
                 # Pad dataframes of geometry results with repeat number:
                 curr_geo_hidden_pre['layer'] = ['hidden_pre']*curr_geo_hidden_pre.shape[0]
