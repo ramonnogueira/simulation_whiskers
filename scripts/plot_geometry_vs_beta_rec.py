21bb20e2699352c60b9848ac40d266b152dbfb92
Daniel Kato <daniel.d.kato@gmail.com>
2024-08-31 08:59:57 -0400

    Add scripts/plot_geometry_vs_beta_rec.py

---
+++ scripts/plot_geometry_vs_beta_rec.py
@@ -0,0 +1,235 @@
+# -*- coding: utf-8 -*-
+"""
+Plot CCGP/parallelism vs reconstruction loss. 
+
+Created on Wed Jul 17 02:14:59 2024
+
+@author: danie
+"""
+
+import sys
+import os
+import pathlib 
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import pickle
+try:
+    from analysis_metadata.analysis_metadata import Metadata, write_metadata, increment_dir_name, seconds_2_full_time_str
+except ImportError or ModuleNotFoundError:
+    analysis_metdata_imported=False
+
+# Input parameters:
+input_path = 'C:\\Users\\danie\\Documents\\code_libraries\\simulation_whiskers\\results\\run605\\ae_iterate_beta_reconstruction.pickle'
+
+# Output parameters:
+save_output = False
+
+
+
+###############################################################################
+#Preliminary stuff:
+
+# Load results:
+results = pickle.load(open(input_path, 'rb'))
+geo_df = results['geo_df']
+geo_df = geo_df[geo_df.beta_sp==0]
+geo_df_hidden = geo_df[geo_df.layer=='hidden'] 
+geo_df_input = geo_df[geo_df.layer=='input'] 
+
+
+"""
+# Compute means:
+Mu = geo_df_hidden[['beta_xor', 'dichotomy_idx', 'train_partition_inds', 'repeat', 'subsample', 'n_hidden', 'train_accuracy', 'test_accuracy']]\
+    .groupby(['beta_xor', 'dichotomy_idx', 'train_partition_inds', 'repeat', 'subsample', 'n_hidden']).mean()\
+    .groupby(['beta_xor', 'dichotomy_idx', 'train_partition_inds', 'repeat', 'n_hidden']).mean()\
+    .groupby(['beta_xor', 'dichotomy_idx', 'train_partition_inds', 'n_hidden']).mean()\
+    .groupby(['beta_xor', 'dichotomy_idx', 'n_hidden']).mean()\
+    .groupby(['beta_xor', 'n_hidden']).mean().reset_index()
+
+
+# Compute standard deviations:
+Std = geo_df_hidden[['beta_xor', 'dichotomy_idx', 'train_partition_inds', 'repeat', 'subsample', 'n_hidden', 'train_accuracy', 'test_accuracy']]\
+    .groupby(['beta_xor', 'dichotomy_idx', 'train_partition_inds', 'repeat', 'subsample', 'n_hidden']).mean()\
+    .groupby(['beta_xor', 'dichotomy_idx', 'train_partition_inds', 'repeat', 'n_hidden']).mean()\
+    .groupby(['beta_xor', 'dichotomy_idx', 'train_partition_inds', 'n_hidden']).mean()\
+    .groupby(['beta_xor', 'dichotomy_idx', 'n_hidden']).mean()\
+    .groupby(['beta_xor', 'n_hidden']).std().reset_index()
+"""
+
+
+# Get some general stuff that will be useful for plotting:
+    
+# Get dichotomies:
+dichotomy_strs = np.unique(geo_df_hidden.dichotomy)
+dichotomy_lines = []
+for i, d in enumerate(dichotomy_strs):
+    curr_dichotomy_str = 'Task {} : {}'.format(i, d)
+    dichotomy_lines.append(curr_dichotomy_str)
+dichotomy_lines = '\n'.join(dichotomy_lines)
+
+# Get number of subsamples/repeats:
+n_subsamples = len(np.unique(geo_df_hidden.subsample))
+n_repeats = len(np.unique(geo_df_hidden.repeat))
+resamples_lines = 'n subsamples = {}, n repeats = {}'.format(n_subsamples, n_repeats)
+
+
+
+
+###############################################################################
+# CCGP analysis:
+
+# Compute CCGP means:
+Mu = geo_df_hidden[['n_hidden', 'dichotomy_idx', 'train_partition_inds', 'repeat', 'subsample', 'beta_rec', 'train_accuracy', 'test_accuracy']]\
+    .groupby(['n_hidden', 'dichotomy_idx', 'train_partition_inds', 'subsample', 'repeat', 'beta_rec']).mean()\
+    .groupby(['n_hidden', 'dichotomy_idx', 'subsample', 'repeat', 'beta_rec']).mean()\
+    .groupby(['n_hidden', 'subsample', 'repeat', 'beta_rec']).mean()\
+    .groupby(['n_hidden', 'subsample', 'beta_rec']).mean()\
+    .groupby(['n_hidden', 'beta_rec']).mean().reset_index()
+
+
+# Compute CCGP standard deviations:
+Std = geo_df_hidden[['n_hidden', 'dichotomy_idx', 'train_partition_inds', 'repeat', 'subsample', 'beta_rec', 'train_accuracy', 'test_accuracy']]\
+    .groupby(['n_hidden', 'dichotomy_idx', 'train_partition_inds', 'subsample', 'repeat', 'beta_rec']).mean()\
+    .groupby(['n_hidden', 'dichotomy_idx', 'subsample', 'repeat', 'beta_rec']).mean()\
+    .groupby(['n_hidden', 'subsample', 'repeat', 'beta_rec']).mean()\
+    .groupby(['n_hidden', 'subsample', 'beta_rec']).mean()\
+    .groupby(['n_hidden', 'beta_rec']).std().reset_index()
+
+# Compute input CCGP for reference:
+Mu_input = geo_df_input[['n_hidden', 'dichotomy_idx', 'train_partition_inds', 'repeat', 'subsample', 'beta_rec', 'train_accuracy', 'test_accuracy']]\
+    .groupby(['n_hidden', 'dichotomy_idx', 'train_partition_inds', 'subsample', 'repeat', 'beta_rec']).mean()\
+    .groupby(['n_hidden', 'dichotomy_idx', 'subsample', 'repeat', 'beta_rec']).mean()\
+    .groupby(['n_hidden', 'subsample', 'repeat', 'beta_rec']).mean()\
+    .groupby(['n_hidden', 'subsample', 'beta_rec']).mean()\
+    .groupby(['n_hidden', 'beta_rec']).mean()\
+    .groupby(['beta_rec']).mean().reset_index()
+Mu_input = Mu_input.mean(axis=0)
+
+
+# Plot CCGP:
+ccgp_fig, ax = plt.subplots(figsize=(6,6))
+
+# Small hidden layer: 
+"""
+small_hidden_Mu = Mu[Mu.n_hidden==25]
+small_hidden_Std = Std[Std.n_hidden==25]
+plt.errorbar(small_hidden_Mu.beta_rec, small_hidden_Mu.test_accuracy, yerr=small_hidden_Std.test_accuracy, label='n=25, beta_sp=0.0')
+"""
+
+# Large hidden layer:
+lg_hidden_Mu = Mu[Mu.n_hidden==40]
+lg_hidden_Std = Std[Std.n_hidden==40]
+plt.errorbar(3*lg_hidden_Mu.beta_rec, lg_hidden_Mu.test_accuracy, yerr=lg_hidden_Std.test_accuracy, color='orange', label='n=40, beta_sp=0.0')
+
+# Plot input CCGP for reference:
+plt.axhline(y=Mu_input.test_accuracy, color='gray', linewidth=0.75, linestyle='--', label='input')
+
+
+main_title_line = 'CCGP vs reconstruction weight'
+title = '\n'.join([main_title_line, dichotomy_lines, resamples_lines])
+plt.title(title)
+plt.ylabel('CCGP')
+plt.xlabel(r'$\beta_{\text{rec}}')
+plt.legend(frameon=False)
+plt.annotate(r'$\beta_{\text{XOR}} = 1$' + '\n' +  r'$\beta_{0} = \beta_{1} = 0$', [0.2, 0.6], xycoords='figure fraction')
+plt.tight_layout()
+
+
+
+
+###############################################################################
+# Parallelism analysis:
+    
+# Compute parallelism means:
+Mu = geo_df_hidden[['n_hidden', 'dichotomy_idx', 'train_partition_inds', 'repeat', 'subsample', 'beta_rec', 'parallelism']]\
+    .groupby(['n_hidden', 'dichotomy_idx', 'train_partition_inds', 'subsample', 'repeat', 'beta_rec']).mean()\
+    .groupby(['n_hidden', 'dichotomy_idx', 'subsample', 'repeat', 'beta_rec']).mean()\
+    .groupby(['n_hidden', 'subsample', 'repeat', 'beta_rec']).mean()\
+    .groupby(['n_hidden', 'subsample', 'beta_rec']).mean()\
+    .groupby(['n_hidden', 'beta_rec']).mean().reset_index()
+
+
+# Compute parallelism standard deviations:
+Std = geo_df_hidden[['n_hidden', 'dichotomy_idx', 'train_partition_inds', 'repeat', 'subsample', 'beta_rec', 'parallelism']]\
+    .groupby(['n_hidden', 'dichotomy_idx', 'train_partition_inds', 'subsample', 'repeat', 'beta_rec']).mean()\
+    .groupby(['n_hidden', 'dichotomy_idx', 'subsample', 'repeat', 'beta_rec']).mean()\
+    .groupby(['n_hidden', 'subsample', 'repeat', 'beta_rec']).mean()\
+    .groupby(['n_hidden', 'subsample', 'beta_rec']).mean()\
+    .groupby(['n_hidden', 'beta_rec']).std().reset_index()
+
+# Compute input parallelism for reference:
+Mu_input = geo_df_input[['n_hidden', 'dichotomy_idx', 'train_partition_inds', 'repeat', 'subsample', 'beta_rec', 'parallelism']]\
+    .groupby(['n_hidden', 'dichotomy_idx', 'train_partition_inds', 'subsample', 'repeat', 'beta_rec']).mean()\
+    .groupby(['n_hidden', 'dichotomy_idx', 'subsample', 'repeat', 'beta_rec']).mean()\
+    .groupby(['n_hidden', 'subsample', 'repeat', 'beta_rec']).mean()\
+    .groupby(['n_hidden', 'subsample', 'beta_rec']).mean()\
+    .groupby(['n_hidden', 'beta_rec']).mean()\
+    .groupby(['beta_rec']).mean().reset_index()
+Mu_input = Mu_input.mean(axis=0)
+
+
+# Plot parallelism:
+parallelism_fig, ax = plt.subplots(figsize=(6,6))
+
+# Small hidden layer: 
+"""
+small_hidden_Mu = Mu[Mu.n_hidden==25]
+small_hidden_Std = Std[Std.n_hidden==25]
+plt.errorbar(small_hidden_Mu.beta_rec, small_hidden_Mu.parallelism, yerr=small_hidden_Std.parallelism, label='n=25, beta_sp=0.0')
+"""
+
+# Large hidden layer:
+lh_hidden_Mu = Mu[Mu.n_hidden==40]
+lh_hidden_Std = Std[Std.n_hidden==40]
+plt.errorbar(3*lh_hidden_Mu.beta_rec, lh_hidden_Mu.parallelism, yerr=lh_hidden_Std.parallelism, color='orange', label='n=40, beta_sp=0.0')
+
+# Plot input parallelism for reference:
+plt.axhline(y=Mu_input.parallelism, color='gray', linewidth=0.75, linestyle='--', label='input')
+
+
+main_title_line = 'Parallelism vs hidden layer size'
+title = '\n'.join([main_title_line, dichotomy_lines, resamples_lines])
+plt.title(title)
+plt.ylabel('Parallelism score')
+plt.xlabel(r'$\beta_{\text{rec}}$')
+plt.legend(frameon=False)
+plt.annotate(r'$\beta_{\text{XOR}} = 1$' + '\n' + r'$\beta_{0} = \beta_{1} = 0$', [0.2, 0.6], xycoords='figure fraction')
+plt.tight_layout()
+
+
+
+###############################################################################
+# Save output if requested:
+    
+if save_output:
+    
+    # Define current output directory:
+    input_dir = os.path.split(input_path)[0]
+    if 'analysis_metadata' in sys.modules:
+        curr_output_dir = increment_dir_name(input_dir, 'geometry_vs_hidden_layer_size')
+    else: 
+        curr_output_dir = os.path.join(input_dir, 'geometry_vs_hidden_layer_size')
+    
+    # Create current output directory if necessary:
+    if not os.path.exists(curr_output_dir):
+        pathlib.Path(curr_output_dir).mkdir(parents=True,exist_ok=True)
+    
+    # Save figures:
+    plt.figure(ccgp_fig)
+    ccgp_fig_path = os.path.join(curr_output_dir, 'ccgp_v_hidden_layer_size_beta_sp0.png')
+    plt.savefig(ccgp_fig_path)
+    
+    plt.figure(parallelism_fig)
+    parallelism_fig_path = os.path.join(curr_output_dir, 'parallelism_v_hidden_layer_size_beta_sp0.png')
+    plt.savefig(parallelism_fig_path)
+    
+    # Save metadata:
+    if 'analysis_metadata' in sys.modules:
+        
+        M = Metadata()
+        M.add_input(input_path)
+        M.add_output(ccgp_fig_path)
+        M.add_output(parallelism_fig_path)
+        metadata_path = os.path.join(curr_output_dir, 'geometry_vs_hidden_layer_size_metadata_beta_sp0.json')
+        write_metadata(M, metadata_path)
