6fece4c6835c752a6534699c474205afcaab9d30
Daniel Kato <daniel.d.kato@gmail.com>
2024-09-06 02:35:35 -0400

    Fix mistake reformatting training and test data for predcition

--- miscellaneous_sparseauto.py
+++ miscellaneous_sparseauto.py
@@ -493,10 +493,10 @@ def iterate_fit_autoencoder(sim_params, tasks, n_files, autoencoder_params=None,
             elif rec_network_type=='predictive':
                 model=prediction_network(n_inp=n_predictor_bins, n_hidden=n_hidden, n_out=n_predicted_bins, sigma_init=sig_init)
           
-                F_train_tgt_torch = F_train_torch[:,n_predictor_bins*n_feat:(n_predictor_bins+1)*n_feat]
+                F_train_tgt_torch = F_train_torch[:,n_predictor_bins*n_feat:(n_predictor_bins+n_predicted_bins)*n_feat]
                 F_train_torch = F_train_torch[:,0:n_predicted_bins*n_feat]
                 
-                F_test_tgt_torch = F_test_torch[:,n_predictor_bins*n_feat:(n_predictor_bins+1)*n_feat]
+                F_test_tgt_torch = F_test_torch[:,n_predictor_bins*n_feat:(n_predictor_bins+n_predicted_bins)*n_feat]
                 F_test_torch = F_train_torch[:,0:n_predicted_bins*n_feat]
             
             # Move variables to graphics card if requested:
