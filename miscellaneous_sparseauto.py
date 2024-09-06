32421eff7af3d409dc6fa4c72ea64c484ea774f4
Daniel Kato <daniel.d.kato@gmail.com>
2024-09-06 13:03:49 -0400

    Eliminate superfluous call to super() in prediction_network.__init__()

--- miscellaneous_sparseauto.py
+++ miscellaneous_sparseauto.py
@@ -1084,7 +1084,6 @@ class sparse_autoencoder_3(sparse_autoencoder):
 
 class prediction_network(nn.Module):
     def __init__(self,n_inp,n_hidden,n_out,sigma_init,k=[2,2],xor=False):    
-        super(sparse_autoencoder,self).__init__()
         self.n_inp=n_inp
         self.n_hidden=n_hidden
         self.n_out=n_out
