9de4b67ded19f560e119d38ffb046ae9eb017243
Daniel Kato <daniel.d.kato@gmail.com>
2024-09-26 21:34:56 -0400

    Save test labels to output of iterate_fit_autoencoder()

--- miscellaneous_sparseauto.py
+++ miscellaneous_sparseauto.py
@@ -602,6 +602,7 @@ def iterate_fit_autoencoder(sim_params, tasks, n_files, autoencoder_params=None,
             curr_ae_results_dict['loss_epochs'] = [ae['loss_vec']]
             curr_ae_results_dict['hidden_rep'] = [hidden_rep]
             curr_ae_results_dict['reconstructed_rep'] = [rec_rep]
+            curr_ae_results_dict['labels'] = [test_labels]
             curr_ae_results_dict['repeat'] = [k]
             curr_ae_df = pd.DataFrame.from_dict(curr_ae_results_dict)
         
