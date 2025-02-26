eebc8f4c9b13c48c21e3f11b25990ad26759e111
Daniel Kato <daniel.d.kato@gmail.com>
2025-02-26 00:20:07 -0500

    Start adding code for more concisely iterating over hyperparameters
    Leaving commented out for now, will comment in after more testing

--- scripts/iterate_ae.py
+++ scripts/iterate_ae.py
@@ -16,7 +16,7 @@ import pandas as pd
 import json
 import itertools
 from simulation_whiskers.simulate_task import load_sim_params, load_task_def
-from simulation_whiskers.miscellaneous_sparseauto import iterate_fit_autoencoder, fmt_ae_metadata
+from simulation_whiskers.miscellaneous_sparseauto import iterate_fit_autoencoder, fmt_ae_metadata, generate_hparams_df
 #from simulation_whiskers.plot import plot_iterate_autoencoder_results, plot_autoencoder_geometry
 from simulation_whiskers.plot import plot_iterate_autoencoder_results, plot_ccgps_by_layer, plot_pars_by_layer
 from analysis_metadata.analysis_metadata import Metadata, increment_dir_name, write_metadata
@@ -72,7 +72,7 @@ task1_def_path='C:\\Users\\danie\\Documents\\\\code_libraries\\simulation_whiske
 
 
 # Define classifier tasks:
-tasks = [
+task_defs = [
     
     # Task 0:
     [
@@ -98,6 +98,53 @@ sig_init = 1.0
 save_learning = False
 chunked_reconstruction_loss = False
 
+# Define simulation parameters:
+concavity = [0]
+n_whisk = 2
+prob_poiss = 1.01
+noise_w = 0.3
+spread = 'auto'
+speed = 2.0
+ini_phase_m = 0
+ini_phase_spr = 100
+delay_time = 0
+freq_m = 3.0
+freq_std = 0.1
+std_reset = 0
+t_total = 2
+dt = 0.1
+dx = 0.01
+n_trials_pre = 50
+n_files = 2
+amp = 2
+freq_sh = [2, 15]
+z1 = [4]
+max_rad = 50
+n_rad = 4
+disp = 4.5
+theta = [0]
+steps_mov = [10, 17]
+rad_vec = [6]
+init_position = 0
+
+# Autoencoder parameters:
+mdl_type = "prediction"
+n_hidden = 10
+sig_init = 1 
+sig_neu = 0.1 
+lr = 0.001
+beta0 = 0
+beta1 = 0
+beta_rec = 0
+beta_xor = 1.0
+n_epochs = 10
+batch_size = 10
+beta_sp = 0
+p_norm = 2
+n_splits = 5
+n_predictor_bins = 10
+n_predicted_bins = 4
+    
 # Compute parameters:
 gpu = False
 
@@ -141,6 +188,30 @@ autoencoder_params=json.load(open(ae_params_path,'r'))
 
 
 #%%
+
+simulation_params = ['concavity', 'n_whisk', 'prob_poiss', 'noise_w', 'spread',
+     'speed', 'ini_phase_m', 'ini_phase_spr', 'delay_time', 'freq_m', 'freq_std',
+     'std_reset', 't_total', 'dt', 'dx', 'n_trials_pre', 'n_files', 'amp', 'freq_sh',
+     'z1', 'max_rad', 'n_rad', 'disp', 'theta', 'steps_mov', 'rad_vec', 'init_position']
+
+autoencoder_params = ['mdl_type', 'n_hidden', 'sig_init', 'sig_neu', 'lr', 'beta0',
+    'beta1', 'beta_rec', 'beta_xor', 'n_epochs', 'batch_size', 'beta_sp', 'p_norm',
+    'n_splits', 'n_predictor_bins', 'n_predicted_bins']
+
+hparams_df = generate_hparams_df(hparams=None, task_defs=task_defs, n_files=n_files, 
+     xor=xor, n_geo_subsamples=n_geo_subsamples, zscore_data=zscore_data, 
+     save_perf=False, sum_inpt=sum_inpt, chunked_rec=False, save_learning=save_learning, 
+     gpu=gpu, save_sessions=False, verbose=False, concavity=concavity, n_whisk=n_whisk, 
+     prob_poiss=prob_poiss, noise_w=noise_w, spread=spread, speed=speed, 
+     ini_phase_m=ini_phase_m, ini_phase_spr=ini_phase_spr, delay_time=delay_time, 
+     freq_m=freq_m, freq_std=freq_std, std_reset=std_reset, t_total=t_total, dt=dt, 
+     dx=dx, n_trials_pre=n_trials_pre, n_repeats=n_files, amp=amp, freq_sh=freq_sh, 
+     z1=z1, max_rad=max_rad, n_rad=n_rad, disp=disp, theta=theta, steps_mov=steps_mov, 
+     rad_vec=rad_vec, init_position=init_position, mdl_type=mdl_type, n_hidden=n_hidden, 
+     sig_init=sig_init, sig_neu=sig_neu, lr=lr, beta0=beta0, beta1=beta1, beta_rec=beta_rec, 
+     beta_xor=beta_xor, beta_sp=beta_sp, n_epochs=n_epochs, batch_size=batch_size, 
+     p_norm=p_norm, n_splits=n_splits, n_predictor_bins=n_predictor_bins, 
+     n_predicted_bins=n_predicted_bins)
 
 
 # Verify parameters before executing:
@@ -259,6 +330,44 @@ for spath in sim_params_paths:
                 curr_ae_results['sig_init'] = [sig_init]*curr_ae_results.shape[0]    
                 all_ae_results = pd.concat([all_ae_results, curr_ae_results])
             #"""
+
+"""
+for hidx, hparams in hparams_df.iterrows():
+    
+    curr_sim_params = dict(hparams[simulation_params])
+    curr_autoencoder_params = dict(hparams[autoencoder_params])
+    
+    curr_results=iterate_fit_autoencoder(curr_sim_params,  
+        tasks=hparams.task_defs, n_files=1, autoencoder_params=curr_autoencoder_params, 
+        xor=hparams.xor, n_geo_subsamples=hparams.n_geo_subsamples, zscore_data=hparams.zscore_data, 
+        save_perf=False, sum_inpt=hparams.sum_inpt, chunked_rec=hparams.chunked_reconstruction_loss, 
+        save_learning=hparams.save_learning, gpu=hparams.gpu, save_sessions=False, 
+        verbose=True)
+
+    curr_hparams_df = pd.DataFrame(hparams_df.iloc[0]).T
+
+    # Extract geometry results, add metadata:
+    curr_geo_results = curr_results['geo_df']
+    geo_meta_cols = set(curr_hparams_df) - set(curr_geo_results.columns)
+    geo_meta = pd.concat([curr_hparams_df[geo_meta_cols]]*curr_geo_results.shape[0],axis=0)
+    curr_geo_results = pd.concat([curr_geo_results, geo_meta], axis=1)
+    all_geo_results = pd.concat([all_geo_results, curr_geo_results], axis=0)
+    
+    # Extract classifier performance results, add metadata:
+    curr_perf_results = curr_results['perf_df']
+    perf_meta_cols = set(curr_hparams_df) - set(curr_perf_results.columns)
+    perf_meta = pd.concat([curr_hparams_df[perf_meta_cols]]*curr_perf_results.shape[0],axis=0)
+    curr_perf_results = pd.concat([curr_perf_results, perf_meta], axis=1)
+    all_perf_results = pd.concat([all_perf_results, curr_perf_results], axis=0)
+
+    # Extract autoencoder representations, add metadata:
+    if curr_results['ae_df'] is not None:            
+        curr_ae_results = curr_results['ae_df']
+        ae_meta_cols = set(curr_hparams_df) - set(curr_ae_results.columns)
+        ae_meta = pd.concat([curr_hparams_df[ae_meta_cols]]*curr_ae_results.shape[0],axis=0)
+        curr_ae_results = pd.concat([curr_ae_results, perf_meta], axis=1)
+        all_ae_results = pd.concat([all_ae_results, curr_ae_results])
+"""        
 
 all_results = dict()
 all_results['geo_df'] = all_geo_results
